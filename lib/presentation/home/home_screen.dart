ea755b7ea4199df9c58064a33f61930b001193b9
VuPham <phamtuanvu250396@gmail.com>
2025-11-11 01:24:34 +0700

    Report Widget

--- lib/presentation/home/home_screen.dart
+++ lib/presentation/home/home_screen.dart
@@ -1,3 +1,8 @@
+import 'package:bnv_opendata/presentation/home/report_widget/report_widget.dart';
+import 'package:bnv_opendata/presentation/widgets/app_scaffold.dart';
+import 'package:bnv_opendata/resources/generated/l10n/App_localizations.dart';
+import 'package:bnv_opendata/widgets/xela_widgets/xela_color.dart';
+import 'package:bnv_opendata/widgets/xela_widgets/xela_text_style.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -5,7 +10,10 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const _HomeScreenBody();
+    return const AppScaffold(
+      body: _HomeScreenBody(),
+      bgColor: XelaColor.Gray12,
+    );
   }
 }
 
@@ -14,7 +22,46 @@ class _HomeScreenBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return ListView(
+      children: [
+        const ReportWidget(),
+        const SizedBox(height: 16,),
+        Container(
+          padding: const EdgeInsets.all(16),
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(16),
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Text(
+                AppS.of(context).warning_list,
+                style: XelaTextStyle.XelaSubheadline.apply(color: XelaColor.Gray2),
+              ),
+              const Text('Content')
+            ],
+          ),
+        ),
+        const SizedBox(height: 16,),
+        Container(
+          padding: const EdgeInsets.all(16),
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(16),
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Text(
+                'Tiến độ',
+                style: XelaTextStyle.XelaSubheadline.apply(color: XelaColor.Gray2),
+              ),
+              const Text('Content ...')
+            ],
+          ),
+        ),
+      ],
+    );
   }
 }
-
